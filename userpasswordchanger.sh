95790e2f33be1b3974cbe8ca93c4861d08a62559
shaply <68869959+shaply@users.noreply.github.com>
2020-08-03 12:32:57 -0500

    Update userpasswordchanger.sh

--- userpasswordchanger.sh
+++ userpasswordchanger.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+#Download needed stuff
+apt-get upgrade
+apt-get update
+apt-get vim
+apt install python3
+
+#Edit user passwords
 USERS=$(getent passwd {1000..6000} | cut -d: -f1)
 apt install whois
 EncryptedPassword=$(mkpasswd -m sha-512 T3@m_C7Ickb@it)
