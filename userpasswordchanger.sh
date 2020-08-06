1b277e8e8bf5a05964c0ab26516574d62a4d68ea
shaply <68869959+shaply@users.noreply.github.com>
2020-08-06 12:17:17 -0500

    Update userpasswordchanger.sh

--- userpasswordchanger.sh
+++ userpasswordchanger.sh
@@ -13,3 +13,12 @@ EncryptedPassword=$(mkpasswd -m sha-512 T3@m_C7Ickb@it)
 for USER in $USERS; do
 usermod -p $EncryptedPassword $USER ;
 done
+
+#https://github.com/Forty-Bot/linux-checklist   number 7
+sudo ufw enable
+sysctl -n net.ipv4.tcp_syncookies
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo 0 | sudo tee /proc/sys/net/ipv4/ip_forward
+echo "nospoof on" | sudo tee -a /etc/host.conf
+sudo apt-get update
+sudo apt-get upgrade
